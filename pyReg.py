867078d556a9983d567ee15de2c50247d10c6fa8
Michael Chavinda <mchavinda@colgate.edu>
2015-11-10 01:22:05 -0500

    Modularised the code more.

--- pyReg.py
+++ pyReg.py
@@ -2,10 +2,12 @@
 import bs4
 import requests
 import itertools
+import getpass
 
 ## temporary file that stores the page so I can later open in
 ## and analyse its contents
 f = open('afterLog.html', 'w')
+final_reg_URL = "https://bannersv04.colgate.edu/prod/bwckcoms.P_Regs"
 
 def login_to_portal(session, username, password):
     LOGIN_URL = 'https://cas.colgate.edu/cas/login'
@@ -28,27 +30,61 @@ def login_to_portal(session, username, password):
         '_eventId': 'submit',
     }
     r = session.post(post_url, data=data)
+    return session
     
-def reg_courses(username, password, pin):
+def reg_courses(username, password, pin, crns):
     '''
     register for courses. TODO: Add more parameters
     '''
     session = requests.Session()
-    login_to_portal(session, username, password)
+    session = login_to_portal(session, username, password)
+    session = login_to_banner(session, pin)
+    reg_data = {'REG_BTN': "Submit Changes",
+				'CRN_IN' : crns}
+				
+    r = session.post(final_reg_URL, data=reg_data)
+    return session
+
+def login_to_banner(session, pin):
     r = session.get('http://bannersv04.colgate.edu:10003/ssomanager/c/SSB')
     REGISTRATION_URL = "https://bannersv04.colgate.edu/prod/bwskfreg.P_AltPin"
-    
     # term data to pass into selection form
     form_data = {
         'term_in': 201502
     }
-    r = session.post(REGISTRATION_URL, data=form_data)
     
+    r = session.post(REGISTRATION_URL, data=form_data)
     # alternate pin
-    form_data = {
-        'pin': int(pin), 
+    pin_data = {
+        'pin': pin, 
     }
+    
     PIN_URL = "https://bannersv04.colgate.edu/prod/bwskfreg.P_CheckAltPin"
-    r = session.post(PIN_URL, data=form_data)
+    r = session.post(PIN_URL, data=pin_data)
     f.write(r.text.encode("utf-8"))
     return session
+
+def get_crns():
+    crns = []
+    i = 1
+    while True:
+        crn = raw_input(str(i) + ") Enter CRN (Press enter when done.)- ")
+        if crn.strip() == "":
+			print "Done."
+			break
+        try:
+            crns.append(str(int(crn.strip())))
+        except:
+            print "CRNs must be numbers only."
+        i += 1
+    return crns
+
+def main():
+    username = raw_input("Enter your Colgate Username: ")
+    password = getpass.getpass("Enter you Colgate account password: ")
+    reg_pin = raw_input("Enter your registration PIN: ")
+    crns = get_crns()
+    reg_courses(username, password, reg_pin, crns)
+    
+if __name__ == "__main__":
+    main()
---
+++ scrapePage.py
@@ -0,0 +1,6 @@
+import bs4
+
+f = open("Add_Drop Classes.html", "r")
+soup = bs4.BeautifulSoup(f)
+
+
